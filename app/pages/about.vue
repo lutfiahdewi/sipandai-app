3e342c127b84e20933c7f31b875d945374cc6762
lutfiahdewi <lutfiahdewi909@gmail.com>
2025-09-02 08:28:50 +0800

    about

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,5 +1,67 @@
 <script lang="ts">
+import { NuxtLink } from '#components';
+
 </script>
 <template>
-    <a href="https://www.flaticon.com/free-icons/population" title="population icons">Population icons created by Freepik - Flaticon</a>
+  <div class="flex flex-col min-h-[70vh] py-4 sm:py-8">
+    <div class="general mb-4 sm:mb-8">
+      <h3 class="text-blue-500 font-bold sm:text-2xl">Informasi Umum BPS</h3>
+      <p>
+        Badan Pusat Statistik adalah Lembaga Pemerintah Non Kementerian yang
+        bertanggung jawab langsung kepada Presiden. Sebelumnya, BPS merupakan
+        Biro Pusat Statistik, yang dibentuk berdasarkan UU Nomor 6 Tahun 1960
+        tentang Sensus dan UU Nomer 7 Tahun 1960 tentang Statistik. Sebagai
+        pengganti kedua UU tersebut ditetapkan UU Nomor 16 Tahun 1997 tentang
+        Statistik. Berdasarkan UU ini yang ditindaklanjuti dengan peraturan
+        perundangan dibawahnya, secara formal nama Biro Pusat Statistik diganti
+        menjadi Badan Pusat Statistik.
+      </p>
+      <p>
+        Materi yang merupakan muatan baru dalam UU Nomor 16 Tahun 1997, antara
+        lain :
+      </p>
+      <ul class="list-disc ps-4">
+        <li>
+          Jenis statistik berdasarkan tujuan pemanfaatannya terdiri atas
+          statistik dasar yang sepenuhnya diselenggarakan oleh BPS, statistik
+          sektoral yang dilaksanakan oleh instansi Pemerintah secara mandiri
+          atau bersama dengan BPS, serta statistik khusus yang diselenggarakan
+          oleh lembaga, organisasi, perorangan, dan atau unsur masyarakat
+          lainnya secara mandiri atau bersama dengan BPS.
+        </li>
+        <li>
+          Hasil statistik yang diselenggarakan oleh BPS diumumkan dalam Berita
+          Resmi Statistik (BRS) secara teratur dan transparan agar masyarakat
+          dengan mudah mengetahui dan atau mendapatkan data yang diperlukan.
+        </li>
+        <li>Sistem Statistik Nasional yang andal, efektif, dan efisien.</li>
+        <li>
+          Dibentuknya Forum Masyarakat Statistik sebagai wadah untuk menampung
+          aspirasi masyarakat statistik, yang bertugas memberikan saran dan
+          pertimbangan kepada BPS.
+        </li>
+      </ul>
+    </div>
+    <div class="content mb-4 sm:mb-8">
+      <h3 class="text-blue-500 font-bold sm:text-2xl">Tentang SIPANDAI</h3>
+      <p>
+        SIPANDAI merupakan gagasan kreatif yang dibuat untuk mengatasi
+        permasalahan Belum Efisiennya Pengelolaan Dokumentasi Berkas Elektronik
+        Tahun 2025.
+      </p>
+      <p>Panduan penggunaan dapat diakses di: 
+        <a to="https://www.canva.com/design/DAGxMi8FRn4/r0yJ7piSPNY-qXG9rvNbsg/edit?utm_content=DAGxMi8FRn4&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank" class="text-blue-600 underline">
+            Link
+        </a>
+      </p>
+      <a
+        href="https://www.flaticon.com/free-icons/population"
+        title="population icons"
+        target="_blank"
+        class="text-blue-600 underline"
+        >Population icons created by Freepik - Flaticon</a
+      >
+    </div>
+    <div class="flex-1"></div>
+  </div>
 </template>
